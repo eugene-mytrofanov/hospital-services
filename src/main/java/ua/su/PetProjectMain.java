32883cc0b1b70a11d1596e9922803decd7026852
Eugene <eugene.validation@gmail.com>
2019-05-17 09:12:13 +0300

    add PetProjectMain, gradle files

---
+++ src/main/java/ua/su/PetProjectMain.java
@@ -0,0 +1,11 @@
+package ua.su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectMain {
+  public static void main(final String[] args) {
+    SpringApplication.run(PetProjectMain.class, args);
+  }
+}
