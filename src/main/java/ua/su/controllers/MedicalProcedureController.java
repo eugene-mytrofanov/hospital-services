f4ce2363b7fa7690fe82fe72392e5aa3ce9b69b8
Eugene <eugene.validation@gmail.com>
2019-06-20 17:58:21 +0300

    controller refactoring

--- src/main/java/ua/su/controllers/MedicalProcedureController.java
+++ src/main/java/ua/su/controllers/MedicalProcedureController.java
@@ -1,5 +1,6 @@
 package ua.su.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,34 +15,35 @@ public class MedicalProcedureController {
 
     private final MedicalProcedureService medicalProcedureService;
 
+    @Autowired
     public MedicalProcedureController(MedicalProcedureService medicalProcedureService) {
         this.medicalProcedureService = medicalProcedureService;
     }
 
-    @GetMapping(value = "/medical-procedures")
+    @GetMapping(value = "/medical_procedures")
     public List<MedicalProcedure> getMedicalProcedures() {
         return medicalProcedureService.findAll();
     }
 
-    @GetMapping(value = "/medical-procedures/{id}")
+    @GetMapping(value = "/medical_procedures/{id}")
     public MedicalProcedure getMedicalProcedure(@PathVariable Long id) {
         return medicalProcedureService.getEntry(id);
     }
 
-    @PostMapping(value = "/clinics/{clinicId}/medical-procedures")
+    @PostMapping(value = "/clinics/{clinicId}/medical_procedures")
     @ResponseStatus(HttpStatus.CREATED)
     public MedicalProcedure createMedicalProcedure(@RequestBody MedicalProcedure medicalProcedure, @PathVariable Long clinicId) {
         return medicalProcedureService.addEntry(medicalProcedure, clinicId);
     }
 
-    @DeleteMapping(value = "/medical-procedures/{id}")
+    @DeleteMapping(value = "/medical_procedures/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteMedicalProcedure(@PathVariable Long id) {
         medicalProcedureService.delete(id);
     }
 
-    @PutMapping(value = "/medical-procedures/{id}")
-    public MedicalProcedure updateMedicalProcedure(@RequestBody MedicalProcedure medicalProcedure, @PathVariable Long id){
+    @PutMapping(value = "/medical_procedures/{id}")
+    public MedicalProcedure updateMedicalProcedure(@PathVariable Long id, @RequestBody MedicalProcedure medicalProcedure) {
         return medicalProcedureService.update(id, medicalProcedure);
     }
 }
